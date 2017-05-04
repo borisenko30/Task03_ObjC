d83a87240a0f0d7e5a817d15bafc892a942c6f68
Student003 <student003@Antons-iMac.local>
2017-05-04 13:42:55 +0300

    Initial Commit

---
+++ Task03_ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task03_ObjC
+//
+//  Created by Student003 on 5/4/17.
+//  Copyright © 2017 Student003. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
